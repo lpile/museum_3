4121408fdf4b76d8eaf2249d9a6bc6168f7272dd
lpile <logan.pile@gmail.com>
2019-04-03 14:20:59 -0600

    create Patron class and test files

---
+++ test/patron_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+
+class PatronTest < Minitest::Test
+  def setup
+    @bob = Patron.new("Bob", 20)
+  end
+
+  def test_patron_class_exist
+    assert_instance_of Patron, @bob
+  end
+
+  def test_patron_class_has_attributes
+    assert_equal "Bob", @bob.name
+    assert_equal 20, @bob.spending_money
+    assert_equal [], @bob.interests
+  end
+
+  def test_add_interests_method_adds_into_interests_array
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @bob.interests
+  end
+end
