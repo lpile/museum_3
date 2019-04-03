aeb2360c91bb6118b4589f506f08dec996b8d792
lpile <logan.pile@gmail.com>
2019-04-03 14:39:51 -0600

    create Museum class and test files

---
+++ lib/museum.rb
@@ -0,0 +1,16 @@
+class Museum
+  attr_reader :name, :exhibits
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+  end
+
+  def add_exhibit(exhibit)
+    @exhibits << exhibit
+  end
+
+  def recommend_exhibits(patron)
+    @exhibits.select {|i| i if patron.interests.include?(i.name)}
+  end
+end
---
+++ test/museum_test.rb
@@ -0,0 +1,46 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/exhibit'
+require './lib/patron'
+require './lib/museum'
+
+class MuseumTest < Minitest::Test
+  def setup
+    @dmns = dmns = Museum.new("Denver Museum of Nature and Science")
+    @gems_and_minerals = Exhibit.new("Gems and Minerals", 0)
+    @dead_sea_scrolls = Exhibit.new("Dead Sea Scrolls", 10)
+    @imax = Exhibit.new("IMAX", 15)
+    @bob = Patron.new("Bob", 20)
+    @sally = Patron.new("Sally", 20)
+
+  end
+
+  def test_museum_class_exist
+    assert_instance_of Museum, @dmns
+  end
+
+  def test_museum_class_has_attributes
+    assert_equal "Denver Museum of Nature and Science", @dmns.name
+    assert_equal [], @dmns.exhibits
+  end
+
+  def test_add_exhibit_method_adds_into_exhibits_array
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls, @imax], @dmns.exhibits
+  end
+
+  def test_recommend_exhibits_method_returns_exhibits_that_match_patrons_interest
+    @dmns.add_exhibit(@gems_and_minerals)
+    @dmns.add_exhibit(@dead_sea_scrolls)
+    @dmns.add_exhibit(@imax)
+    @bob.add_interest("Dead Sea Scrolls")
+    @bob.add_interest("Gems and Minerals")
+    @sally.add_interest("IMAX")
+
+    assert_equal [@gems_and_minerals, @dead_sea_scrolls], @dmns.recommend_exhibits(@bob)
+    assert_equal [@imax], @dmns.recommend_exhibits(@sally)
+  end
+end
